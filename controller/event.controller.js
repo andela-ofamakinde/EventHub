dc23f62e4b62153eab636e9104c0bcfef8d58e73
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-20 13:08:32 +0100

    Add joined User function

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -5,23 +5,13 @@ var _ = require('lodash');
 
 require('../models/user.model');
 var User = mongoose.model('User');
-// var JoinedUser = mongoose.model('JoinedUser');
 
 exports.createEvent = function(req, res){
   Event.create(req.body, function(err, event){
     if (err){
       res.send(err);
-    } else {
-        User.findById(event.userId, function(err, user) {
-        if (err) {
-          res.send(err);
-        }
-        user.eventsCreated.push(event._id);
-        user.save(function(err) {
-          res.json(event);
-        });
-      });
     }
+    res.json(event);
   });
 };
 
@@ -29,6 +19,15 @@ exports.getUserEvents = function(req, res) {
   var userid = req.params.userid;
   Event.find({userId: userid}).populate('userId').exec(function(err, events) {
     if (err) {
+      return res.json(err);
+    }
+    res.json(events);
+  });
+};
+
+exports.getEventsJoined = function(req, res) {
+  Event.find({userId: req.params.userid}).populate('userId').exec(function(err, events){
+    if(err){
       return res.json(err);
     }
     res.json(events);
@@ -54,50 +53,30 @@ exports.getOneEvent = function(req, res) {
 };
 
 exports.updateEvent = function(req, res) {
-  Event.findById(req.params.event_id, function(err , event) {
-    if(err){
+  Event.update({_id: req.params.event_id}, req.body, function(err, events) {
+    if(err) {
       res.send(err);
     }
-
-    event.title = req.body.title;
-    event.description = req.body.description;
-
-    event.save(function(err){
-      if (err){
-        res.send(err);
-      }
-      res.json(event);
-    });
+    res.json(events);
   });
-
 };
 
 exports.joinEvent = function(req, res) {
-
-  Event.findById(req.params.event_id, function(err, events) {
-    
-    if(err) return err;
-    if(events.joinedUsers.length === 0){
-      events.joinedUsers.push(req.body);
-      events.save(function(err){
-        if (err){
-          res.send(err);
-        }
-        res.json(events);
-        });
+  Event.findById(req.params.event_id, function(err, event) {
+    if (err) {
+      res.json(err);
     }
-    else {
-      if(!_.result(_.find(events.joinedUsers, req.body), 'user')){
-            events.joinedUsers.push(req.body);
-            events.save(function(err){
-              if (err){
-                res.send(err);
-              }
-              res.json(events);
-            });
-          }
-          else return res.json('userId exist');
-      }
+    var added = event.joinedUsers.addToSet(req.body.userid);
+    if (!added.length) {
+      res.status(400).json({'message': 'duplicate userid'});
+    } else {
+      event.save(function(err, event) {
+        if (err) {
+          res.json(err);
+        }
+        res.json(event);
+      });
+    }
   });
 };
 
